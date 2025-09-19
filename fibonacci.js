699ee92d9315c940e48af5586dc1905d96b0c652
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-09-19 17:58:09 +0530

    Update to print seven fibonacci number series without loop

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,25 @@
-let numbersToBePrinted = 2;
+let numbersToBePrinted = 7;
 let firstNumber = 0;
 let secondNumber = 1;
-console.log(firstNumber, secondNumber);
+let thirdNumber = firstNumber + secondNumber;
+console.log(firstNumber, secondNumber, thirdNumber);
+
+firstNumber = secondNumber;
+secondNumber = thirdNumber;
+thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber);
+
+firstNumber = secondNumber;
+secondNumber = thirdNumber;
+thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber);
+
+firstNumber = secondNumber;
+secondNumber = thirdNumber;
+thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber);
+
+firstNumber = secondNumber;
+secondNumber = thirdNumber;
+thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber);
