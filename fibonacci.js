f9e7624cf0cb75234eca5d336a3c1bf56dd57bc8
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-09-19 22:45:02 +0530

    Update to print fibonacci series upto terms using for loop

--- fibonacci.js
+++ fibonacci.js
@@ -1,39 +1,10 @@
-let numbersToBePrinted = 6;
+let numbersToBePrinted = 5;
 let firstNumber = 0;
 let secondNumber = 1;
-let thirdNumber = firstNumber + secondNumber;
-let countOfPrintedNumbers = 2;
-console.log(firstNumber, secondNumber);
 
-if(countOfPrintedNumbers <= numbersToBePrinted){
-    console.log(thirdNumber);
+for(let countOfPrintedNumbers = 0; countOfPrintedNumbers < numbersToBePrinted ; countOfPrintedNumbers++){
+    console.log(firstNumber);
+    const followedTerm = firstNumber + secondNumber;
     firstNumber = secondNumber;
-    secondNumber = thirdNumber;
-    thirdNumber = firstNumber + secondNumber;
-    countOfPrintedNumbers = countOfPrintedNumbers + 1;
-}
-
-if(countOfPrintedNumbers <= numbersToBePrinted){
-    console.log(thirdNumber);
-    firstNumber = secondNumber;
-    secondNumber = thirdNumber;
-    thirdNumber = firstNumber + secondNumber;
-    countOfPrintedNumbers = countOfPrintedNumbers + 1;
-}
-
-if(countOfPrintedNumbers <= numbersToBePrinted){
-    console.log(thirdNumber);
-    firstNumber = secondNumber;
-    secondNumber = thirdNumber;
-    thirdNumber = firstNumber + secondNumber;
-    countOfPrintedNumbers = countOfPrintedNumbers + 1;
-
-}
-
-if(countOfPrintedNumbers <= numbersToBePrinted){
-    console.log(thirdNumber);
-    firstNumber = secondNumber;
-    secondNumber = thirdNumber;
-    thirdNumber = firstNumber + secondNumber;
-    countOfPrintedNumbers = countOfPrintedNumbers + 1;
+    secondNumber = followedTerm;
 }
