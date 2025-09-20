69296284f8e9655de421d73b4e606ee4f4e2f609
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-09-20 16:37:26 +0530

    Update to count the number of lions and zebras

--- lion_hunt.js
+++ lion_hunt.js
@@ -1,3 +1,12 @@
-const testCase1 = "LZ";
-const spaceBetweenLionAndZebra = 0;
-console.log("Input: ",testCase1,"Output: ",spaceBetweenLionAndZebra);
+const testCase1 = "Z Z Z";
+const spaceBetweenLionAndZebra = -1;
+let countOfLions = 0;
+let countOfZebra = 0;
+let index = 0;
+
+for (let index = 0; index < testCase1.length; index++) {
+    countOfLions = (testCase1[index] === 'L') ? countOfLions = countOfLions + 1 : countOfLions
+    countOfZebra = (testCase1[index] === 'Z') ? countOfZebra = countOfZebra + 1 : countOfZebra
+}
+
+console.log("count of lions: ", countOfLions, "\n count of zebra: ", countOfZebra);
