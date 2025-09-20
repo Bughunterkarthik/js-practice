22cf73748380e345e7ffff0038378d84b6b58ac8
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-09-20 17:24:40 +0530

    Update to find spaces between a lion and a zebra without using loops
    In this code is repeating

--- lion_hunt.js
+++ lion_hunt.js
@@ -9,12 +9,32 @@ for (let index = 0; index < testCase1.length; index++) {
     countOfLions = (testCase1[index] === 'L') ? countOfLions = countOfLions + 1 : countOfLions
     countOfZebra = (testCase1[index] === 'Z') ? countOfZebra = countOfZebra + 1 : countOfZebra
 }
-for ( let index = 0; index < testCase1.length; index++){
-    if(testCase1[index] === 'L' || testCase1[index] === 'Z'){
+for (let index = 0; index < testCase1.length; index++) {
+    if (testCase1[index] === 'L' || testCase1[index] === 'Z') {
         firstAnimal = index;
         break;
     }
 }
-console.log(firstAnimal);
+if (countOfLions > 0 && countOfZebra > 0) {
+    if (testCaseToUse[index] === " ") {
+        spaceBetweenLionAndZebra = spaceBetweenLionAndZebra + 1;
+    } else {
+        closetSpace = (closetSpace === -1 || spaceBetweenLionAndZebra < closetSpace) ? closetSpace = spaceBetweenLionAndZebra : closetSpace
+        spaceBetweenLionAndZebra = 0;
+    }
+    if (testCaseToUse[index] === " ") {
+        spaceBetweenLionAndZebra = spaceBetweenLionAndZebra + 1;
+    } else {
+        closetSpace = (closetSpace === -1 || spaceBetweenLionAndZebra < closetSpace) ? closetSpace = spaceBetweenLionAndZebra : closetSpace
+        spaceBetweenLionAndZebra = 0;
+    }
+    if (testCaseToUse[index] === " ") {
+        spaceBetweenLionAndZebra = spaceBetweenLionAndZebra + 1;
+    } else {
+        closetSpace = (closetSpace === -1 || spaceBetweenLionAndZebra < closetSpace) ? closetSpace = spaceBetweenLionAndZebra : closetSpace
+        spaceBetweenLionAndZebra = 0;
+    }
+    
+}
 
-console.log("count of lions: ", countOfLions, "\n count of zebra: ", countOfZebra);
+console.log("Input: ", testCase1, "Output: ", spaceBetweenLionAndZebra);
