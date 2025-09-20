50737bd9fc62531334e475e8ebde4bbd55a6dc75
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-09-20 17:32:49 +0530

    Code is successfully exceuting without any errors and giving correct output
    In this code, I provided some testcases in the the beginning of the file. I used a variable to assign one of those variables and perform operation on the variable

--- lion_hunt.js
+++ lion_hunt.js
@@ -1,31 +1,46 @@
-const testCase1 = "L  ZL Z";
-let spaceBetweenLionAndZebra = -1;
+const testCase1 = "LZ";
+const testCase2 = "L Z";
+const testCase3 = "L ZL";
+const testCase4 = "L   Z  L Z";
+const testCase5 = "L  ZL Z";
+const testCase6 = "L     L";
+const testCase7 = "Z   Z   Z";
+const testCase8 = " Z L";
+const testCase9 = "L  L Z  L"
+const testCase10 = "    ";
+
+
 let closetSpace = -1;
+let spaceBetweenLionAndZebra = 0;
+let firstAnimal = 0;
 let countOfLions = 0;
 let countOfZebra = 0;
+const testCaseToUse = testCase8;
 let index = 0;
-let firstAnimal;
 
-for (let index = 0; index < testCase1.length; index++) {
-    countOfLions = (testCase1[index] === 'L') ? countOfLions = countOfLions + 1 : countOfLions
-    countOfZebra = (testCase1[index] === 'Z') ? countOfZebra = countOfZebra + 1 : countOfZebra
+
+while (firstAnimal === 0 && index < testCaseToUse.length) {
+    firstAnimal = (testCaseToUse[index] === 'L' || testCaseToUse[index] === 'Z') ? firstAnimal = index : firstAnimal
+    index = index + 1;
 }
-for (let index = 0; index < testCase1.length; index++) {
-    if (testCase1[index] === 'L' || testCase1[index] === 'Z') {
-        firstAnimal = index;
-        break;
-    }
+
+for (let index = 0; index < testCaseToUse.length; index++) {
+    countOfLions = (testCaseToUse[index] === 'L') ? countOfLions = countOfLions + 1 : countOfLions
+    countOfZebra = (testCaseToUse[index] === 'Z') ? countOfZebra = countOfZebra + 1 : countOfZebra
 }
+
 if (countOfLions > 0 && countOfZebra > 0) {
-    for (let index = firstAnimal + 1; index < testCase1.length; index++){
-        if (testCase1[index] === " ") {
+
+    for (let index = firstAnimal + 1; index < testCaseToUse.length; index++) {
+        if (testCaseToUse[index] === " ") {
             spaceBetweenLionAndZebra = spaceBetweenLionAndZebra + 1;
         } else {
             closetSpace = (closetSpace === -1 || spaceBetweenLionAndZebra < closetSpace) ? closetSpace = spaceBetweenLionAndZebra : closetSpace
             spaceBetweenLionAndZebra = 0;
         }
     }
-    
 }
 
-console.log("Input: ", testCase1, "Output: ", spaceBetweenLionAndZebra);
+
+
+console.log("Input: ", testCaseToUse, "\nOutput: ", closetSpace);
