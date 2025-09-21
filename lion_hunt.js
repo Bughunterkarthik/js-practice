1096b9212f01c074f00ddad74c706e0f2846cabb
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-09-21 10:06:09 +0530

    Added some testcases
    previous code is not working for those testcases

--- lion_hunt.js
+++ lion_hunt.js
@@ -8,34 +8,46 @@ const testCase7 = "Z   Z   Z";
 const testCase8 = " Z L";
 const testCase9 = "L  L Z  L"
 const testCase10 = "    ";
+const testCase11 = "L   ZZ  L";
+const testCase12 = "Z   LL Z";
+const testCase13 = "  L  ZL";
+const testCase14 = "L  Z LL  ";
 
 
 let closetSpace = -1;
 let spaceBetweenLionAndZebra = 0;
-let firstAnimal = 0;
+let firstAnimalLocation = "";
 let countOfLions = 0;
 let countOfZebra = 0;
-const testCaseToUse = testCase8;
-let index = 0;
+let previousAnimalFound;
+const testCaseToUse = testCase14;
+let location = 0;
 
-
-while (firstAnimal === 0 && index < testCaseToUse.length) {
-    firstAnimal = (testCaseToUse[index] === 'L' || testCaseToUse[index] === 'Z') ? firstAnimal = index : firstAnimal
-    index = index + 1;
+// to find first animal location in the string
+while (firstAnimalLocation === "" && location < testCaseToUse.length) {
+    firstAnimalLocation = (testCaseToUse[location] === 'L' || testCaseToUse[location] === 'Z') ? firstAnimalLocation = location : firstAnimalLocation
+    previousAnimalFound = (testCaseToUse[location] === 'L') ? 'L' : 'Z';
+    location = location + 1;
 }
 
-for (let index = 0; index < testCaseToUse.length; index++) {
-    countOfLions = (testCaseToUse[index] === 'L') ? countOfLions = countOfLions + 1 : countOfLions
-    countOfZebra = (testCaseToUse[index] === 'Z') ? countOfZebra = countOfZebra + 1 : countOfZebra
+// to count of lions and zebras in the string
+while ((countOfLions === 0 || countOfZebra === 0) && location < testCaseToUse.length) {
+    countOfLions = (testCaseToUse[location] === 'L') ? countOfLions = countOfLions + 1 : countOfLions
+    countOfZebra = (testCaseToUse[location] === 'Z') ? countOfZebra = countOfZebra + 1 : countOfZebra
+    location = location + 1;
 }
 
+// to find the spaces between a lion and a zebra 
 if (countOfLions > 0 && countOfZebra > 0) {
 
-    for (let index = firstAnimal + 1; index < testCaseToUse.length; index++) {
+    for (let index = firstAnimalLocation + 1; index < testCaseToUse.length; index++) {
         if (testCaseToUse[index] === " ") {
             spaceBetweenLionAndZebra = spaceBetweenLionAndZebra + 1;
         } else {
-            closetSpace = (closetSpace === -1 || spaceBetweenLionAndZebra < closetSpace) ? closetSpace = spaceBetweenLionAndZebra : closetSpace
+            if (testCaseToUse[index] !== previousAnimalFound) {
+                closetSpace = (closetSpace === -1 || spaceBetweenLionAndZebra < closetSpace) ? closetSpace = spaceBetweenLionAndZebra : closetSpace
+                previousAnimalFound = testCaseToUse[index];
+            }
             spaceBetweenLionAndZebra = 0;
         }
     }
