eff57e2058d4846bda58de510b25878a63724fff
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-09-21 16:44:16 +0530

    executing file without any error

--- is_string_palindrome.js
+++ is_string_palindrome.js
@@ -1,15 +1,18 @@
-const stringToBeChecked = "aa";
+const stringToBeChecked = "aaaaa";
 const lengthOfString = stringToBeChecked.length;
 let CountOfEqualCharacters = 0;
 
-for (let stringIndex = 0; stringIndex < lengthOfString; stringIndex++) {
+if (lengthOfString % 2 !== 0){
+    CountOfEqualCharacters = -0.5
+}
+for (let stringIndex = 0; stringIndex < lengthOfString / 2; stringIndex++) {
     if (stringToBeChecked[stringIndex] === stringToBeChecked[lengthOfString - stringIndex - 1]) {
         CountOfEqualCharacters = CountOfEqualCharacters + 1
     }
-    stringIndex = stringIndex + 1
 }
 
 const suffix = (CountOfEqualCharacters === lengthOfString / 2) ? "is a palindrome" : "is not a palindrome"
 
 console.log(stringToBeChecked, suffix);
+
 
