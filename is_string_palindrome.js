b28d5d8a43401d677fdc64c58308462ff7013588
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-09-21 16:01:05 +0530

    checking a given string is palindrome or  not without any hardcoded message
    In this code, some code is repeating multiple times

--- is_string_palindrome.js
+++ is_string_palindrome.js
@@ -1,7 +1,36 @@
-const stringToBeChecked = "madam";
+const stringToBeChecked = "ABCCBA";
 const lengthOfString = stringToBeChecked.length;
 let CountOfEqualCharacters = 0;
+let stringIndex
+
+if(stringToBeChecked[stringIndex] === stringToBeChecked[lengthOfString - stringIndex - 1]){
+    CountOfEqualCharacters = CountOfEqualCharacters +1
+}
+stringIndex = stringIndex + 1
+
+
+if(stringToBeChecked[stringIndex] === stringToBeChecked[lengthOfString - stringIndex - 1]){
+    CountOfEqualCharacters = CountOfEqualCharacters +1
+}
+stringIndex = stringIndex + 1
+
+if(stringToBeChecked[stringIndex] === stringToBeChecked[lengthOfString - stringIndex - 1]){
+    CountOfEqualCharacters = CountOfEqualCharacters +1
+}
+stringIndex = stringIndex + 1
+
+if(stringToBeChecked[stringIndex] === stringToBeChecked[lengthOfString - stringIndex - 1]){
+    CountOfEqualCharacters = CountOfEqualCharacters +1
+}
+stringIndex = stringIndex + 1
 
 const suffix = (CountOfEqualCharacters === lengthOfString ) ? "is a palindrome" : "is not a palindrome"
 
 console.log( stringToBeChecked,suffix);
+
+
+
+
+if (lengthOfString % 2 !== 0){
+    CountOfEqualCharacters = 0.5
+}
